d7092821e2112483c253239f1c1417df2d9f08e1
ads00-crypto <adsnotifications00@gmail.com>
2025-10-04 22:47:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-// server.js
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('✅ API Okta Test Running Successfully!');
+});
+
+app.get('/callback', (req, res) => {
+  res.send('✅ Login successful — authorization code received!');
 });
 
 app.listen(PORT, () => {
