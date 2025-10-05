cf305bf4b330b3434b8fdf08eb40ad4b66e21bdb
ads00-crypto <adsnotifications00@gmail.com>
2025-10-04 23:40:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/login', (_req, res) => res.sendFile(path.join(__dirname, 'login.html')));
-app.get('/callback', (_req, res) => res.sendFile(path.join(__dirname, 'callback.html')));
+// Página principal (inicio)
 app.get('/', (_req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
+app.get('/login', (_req, res) => { res.redirect('/auth');
+                                 });
+
+app.get('/callback', (_req, res) => res.sendFile(path.join(__dirname, 'callback.html')));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
